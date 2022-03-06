7257181cc9a507f7318b61b7918adef7dfdd7c8b
Tarun Bisht <bishttarun2002@gmail.com>
2022-03-06 23:43:14 +0530

    Create Chessboard Distance.cpp

---
+++ February Lunchtime 2022/Chessboard Distance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x1,y1,x2,y2;
+	    cin>>x1>>y1>>x2>>y2;
+	    cout<<max(abs(x1-x2),abs(y1-y2))<<endl;
+	}
+	return 0;
+}

136363de2f6a09f38afe9bf1bcfdb2318cc93d64
Tarun Bisht <bishttarun2002@gmail.com>
2022-03-06 23:48:24 +0530

    Create Janmansh and Assignments.cpp

---
+++ March Cook-Off 2022/Janmansh and Assignments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x>7)
+	    cout<<"No"<<endl;
+	    else
+	    cout<<"Yes"<<endl;
+	}
+	return 0;
+}

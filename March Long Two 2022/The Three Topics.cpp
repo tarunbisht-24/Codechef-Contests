c11bd4f4ebe75eb02470ffebded12e94905c31ed
Tarun Bisht <bishttarun2002@gmail.com>
2022-03-26 07:59:17 +0530

    Create The Three Topics.cpp

---
+++ March Long Two 2022/The Three Topics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	
+	if(x==a || x==b || x==c)
+	cout<<"Yes"<<endl;
+	else
+	cout<<"No"<<endl;
+	return 0;
+}

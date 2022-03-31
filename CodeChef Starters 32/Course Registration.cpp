74789c2987c779365c2ffaba2d794d51a3a07de3
Tarun Bisht <bishttarun2002@gmail.com>
2022-03-31 22:36:41 +0530

    Create Course Registration.cpp

---
+++ CodeChef Starters 32/Course Registration.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m,k;
+	    cin>>n>>m>>k;
+	    if(n+k<=m)
+	    cout<<"Yes"<<endl;
+	    else
+	    cout<<"No"<<endl;
+	}
+	return 0;
+}

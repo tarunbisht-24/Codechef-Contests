29708d32064acf968c41e2e4481594fa97ab4cc5
Tarun Bisht <bishttarun2002@gmail.com>
2022-04-02 22:42:01 +0530

    Create Important Pages on CodeChef.cpp

---
+++ April Cook-Off 2022/Important Pages on CodeChef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	if(a==0 && b==1 || a==0 && b==0)
+	cout<<"https://www.codechef.com/practice"<<endl;
+	else if(a==1 && b==0)
+	cout<<"https://www.codechef.com/contests"<<endl;
+	else
+	cout<<"https://discuss.codechef.com"<<endl;
+	return 0;
+}

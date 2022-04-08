6c52e7e3034eab5aa732cfda46f751b0099fbf02
Tarun Bisht <bishttarun2002@gmail.com>
2022-04-08 16:58:23 +0530

    Create Monthly Budget.cpp

---
+++ CodeChef Starters 33/Monthly Budget.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(y*30<=x)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}

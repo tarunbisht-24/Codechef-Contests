02860849c2dab9079c99c671bc84c1c14c1bcccd
Tarun Bisht <bishttarun2002@gmail.com>
2022-05-14 20:52:25 +0530

    Update Credit score.cpp

--- March Lunchtime 2022/Credit score.cpp
+++ March Lunchtime 2022/Credit score.cpp
@@ -1,3 +1,36 @@
+/*
+To access CRED programs, one needs to have a credit score of 750 or more.
+Given that the present credit score is X, determine if one can access CRED programs or not.
+
+If it is possible to access CRED programs, output YES, otherwise output NO.
+
+Input Format
+The first and only line of input contains a single integer X, the credit score.
+
+Output Format
+Print YES if it is possible to access CRED programs. Otherwise, print NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings YeS, yEs, yes and YES will all be treated as identical).
+
+Constraints
+0≤X≤1000
+Subtasks
+Subtask 1 (100 points): Original constraints.
+Sample Input 1 
+823
+Sample Output 1 
+YES
+Explanation
+Since 823≥750, it is possible to access CRED programs.
+
+Sample Input 2 
+251
+Sample Output 2 
+NO
+Explanation
+Since 251<750, it is not possible to access CRED programs.
+*/
+
 #include <iostream>
 using namespace std;
 
